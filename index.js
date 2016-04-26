fa0955cf831250e9eee06340ad49c7750a33ee23
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-04-26 07:54:45 +0100

    Bunged in router and Express

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+var express =      require('express');
+var bodyParser =   require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/puregympresence');
 
 var scraper = require('./scraper');
+var router = require('./router');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use("/", router);
+
+app.listen(3210);
 
 scraper();
